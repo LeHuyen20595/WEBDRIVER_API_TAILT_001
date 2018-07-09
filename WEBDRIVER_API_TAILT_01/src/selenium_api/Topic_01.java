6ef2bf60a4a712b8aa2cdb83065193d60a1c29ae
Huyen Le <lehuyen20595@gmail.com>
2018-07-09 21:50:24 +0700

    first commit

---
+++ WEBDRIVER_API_TAILT_01/src/selenium_api/Topic_01.java
@@ -0,0 +1,43 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+
+
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class Topic_01 {
+	private WebDriver driver;
+	@BeforeTest
+	public void beforeTest() {
+		// run with firefox 47
+		driver = new FirefoxDriver();
+		
+		// run with chrome
+		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+		driver = (WebDriver) new ChromeDriver();
+		
+		// run with Ie
+		System.setProperty("webdriver.ie.driver", ".\\drivers\\IeDriverServer.exe");
+		driver = new InternetExplorerDriver();
+	}
+
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
